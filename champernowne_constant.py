91438f5ddb2d52b525eccd25afec7cdfcbe64d76
migueleichler <migueleichler23@gmail.com>
2017-11-28 01:36:37 -0200

    Solution for Problem 40 (Champernowne's Constant) from Project Euler implemented.

--- champernowne_constant.py
+++ champernowne_constant.py
@@ -6,19 +6,44 @@
 # the following expression.
 # d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
 from unittest import TestCase
+import math
 
 
 def get_nth_digit(number, digit_position):
-    str_number = str(number)
-    if digit_position > 0 and digit_position <= len(str_number):
-        return int(str_number[digit_position - 1])
-    return None
+    if digit_position > 0 and digit_position <= len(number):
+        return int(number[digit_position - 1])
+    return -1
+
+
+def create_champernowne_constant(last_digit_position):
+    champernowne_constant = ''
+    for i in range(1, last_digit_position + 1):
+        champernowne_constant = champernowne_constant + str(i)
+    return champernowne_constant
+
+
+def champernowne_constant():
+    result = 1
+    power = 1
+    champernowne_constant = create_champernowne_constant(1000000)
+    for i in range(0, len(champernowne_constant) + 1):
+        if i == math.pow(10, power):
+            power += 1
+            result = result * get_nth_digit(champernowne_constant, i)
+
+    return result
 
 
 class Tests(TestCase):
     def setUp(self):
-        self.number = 1210567
+        self.number = '12345678910'
 
     def test_get_nth_digit(self):
-        self.assertEqual(0, get_nth_digit(number=self.number, digit_position=4))
-        self.assertEqual(None, get_nth_digit(number=self.number, digit_position=100))
+        self.assertEqual(4, get_nth_digit(number=self.number, digit_position=4))
+        self.assertEqual(-1, get_nth_digit(number=self.number, digit_position=100))
+
+    def test_create_champernowne_constant(self):
+        self.assertEqual(self.number, create_champernowne_constant(10))
+
+    def test_champernowne_constant(self):
+        self.assertEqual(210, champernowne_constant())
